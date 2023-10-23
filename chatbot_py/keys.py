f3fe8e734e29a3148652d225535a222ebdfc4e73
Cortes <guillem.cortes@gfk.com>
2023-10-23 12:16:12 +0200

    Add keys.py with instructions

---
+++ chatbot_py/keys.py
@@ -0,0 +1,4 @@
+TRELLO_API_KEY = "Replace with your Trello API key"
+TRELLO_API_SECRET = "Replace with your Trello API secret"
+TRELLO_TOKEN = "Replace with your Trello token"
+id_board = "Put the id of the the trello board"
